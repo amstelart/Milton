53b80a1eb2abf3e98f8910e02cd46cded0e136fb
amstelart <amsteldroid@gmail.com>
2018-06-05 15:40:25 +0300

    fix progress 3

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,90 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+
+  // area-slider
+  var rangeSliderArea = document.getElementById('area-slider');
+  var rangeSliderAreaStartInput = document.getElementById('area-slider-start');
+  var rangeSliderAreaEndInput = document.getElementById('area-slider-end');
+  noUiSlider.create(rangeSliderArea, {
+    start: [rangeSliderAreaStartInput.value, rangeSliderAreaEndInput.value],
+    connect: true,
+    step: 1,
+    start: [20, 90],
+    range: {
+      'min': 0,
+      'max': 110
+    }
+  });
+  rangeSliderArea.noUiSlider.on('update', function( values, handle ) {
+    var value = values[handle];
+    if ( handle ) {
+      rangeSliderAreaEndInput.value = Math.round(value);
+    } else {
+      rangeSliderAreaStartInput.value = Math.round(value);
+    }
+  });
+  rangeSliderAreaEndInput.addEventListener('change', function(){
+    rangeSliderArea.noUiSlider.set([null, this.value]);
+  });
+  rangeSliderAreaStartInput.addEventListener('change', function(){
+    rangeSliderArea.noUiSlider.set([this.value, null]);
+  });
+
+  // cost-slider
+  var rangeSliderCost = document.getElementById('cost-slider');
+  var rangeSliderCostStartInput = document.getElementById('cost-slider-start');
+  var rangeSliderCostEndInput = document.getElementById('cost-slider-end');
+  noUiSlider.create(rangeSliderCost, {
+    start: [rangeSliderCostStartInput.value, rangeSliderCostEndInput.value],
+    connect: true,
+    step: 1,
+    start: [20, 90],
+    range: {
+      'min': 0,
+      'max': 110
+    }
+  });
+  rangeSliderCost.noUiSlider.on('update', function( values, handle ) {
+    var value = values[handle];
+    if ( handle ) {
+      rangeSliderCostEndInput.value = Math.round(value);
+    } else {
+      rangeSliderCostStartInput.value = Math.round(value);
+    }
+  });
+  rangeSliderCostEndInput.addEventListener('change', function(){
+    rangeSliderCost.noUiSlider.set([null, this.value]);
+  });
+  rangeSliderCostStartInput.addEventListener('change', function(){
+    rangeSliderCost.noUiSlider.set([this.value, null]);
+  });
+
+
+  // range slider
+  // var rangeSliderArea = document.getElementById('area-slider');
+  // var rangeSliderCost = document.getElementById('cost-slider');
+  //
+  // noUiSlider.create(rangeSliderArea, {
+  //   start: [70, 110],
+  //   connect: true,
+  //   step: 10,
+  //   range: {
+  //     'min': 0,
+  //     'max': 110
+  //   }
+  // });
+  //
+  // noUiSlider.create(rangeSliderCost, {
+  //   start: [70, 100],
+  //   connect: true,
+  //   step: 10,
+  //   range: {
+  //     'min': 0,
+  //     'max': 110
+  //   }
+  // });
+
+});
 
 // Изоляция без jQuery
 // (function(){
